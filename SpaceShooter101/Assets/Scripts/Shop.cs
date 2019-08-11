9b228c0e127c4690cadbaf0c5bff32d8382f887c
Praveen Liyanaarachchi <hello@praveen.one>
2019-08-11 18:54:40 +0900

    Minor Changers

---
+++ SpaceShooter101/Assets/Scripts/Shop.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace praveen.One
+{
+    public class Shop : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnClickHomeButton()
+        {
+            SceneManager.LoadScene("Start", LoadSceneMode.Single);
+        }
+
+        public void OnClickBackToGame()
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+        public void OnClickUpgradeGun()
+        {
+
+        }
+
+        public void OnClickUpgradeRocket()
+        {
+
+        }
+    }
+}
+

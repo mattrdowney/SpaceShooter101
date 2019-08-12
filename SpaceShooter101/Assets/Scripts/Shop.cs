3901f0d49c137c1295ef88b3638f0b29b7c7a140
Praveen Liyanaarachchi <hello@praveen.one>
2019-08-12 17:32:14 +0900

    Shop redesigned

--- SpaceShooter101/Assets/Scripts/Shop.cs
+++ SpaceShooter101/Assets/Scripts/Shop.cs
@@ -58,19 +58,19 @@ namespace praveen.One
 
 
         #region SerializedFields
-        [SerializeField] Text m_GunPowerLevel;
-        [SerializeField] Text m_GunPowerCost;
-        [SerializeField] Text m_RocketLevel;
-        [SerializeField] Text m_RocketCost;
-        [SerializeField] Text m_ShieldCost;
-        [SerializeField] Text m_ShieldLevel;
         [SerializeField] Text m_Coins;
         [SerializeField] Text m_MissileCount;
-        [SerializeField] Text m_MissileCostTxt;
+
+        [SerializeField] Text m_GunPowerInfo;
+        [SerializeField] Text m_MagazineInfo;
+        [SerializeField] Text m_MissileInfoText;
+        [SerializeField] Text m_ShieldInfo;
+        
         [SerializeField] Slider m_GunPowerSlider;
         [SerializeField] Slider m_MagazineSlider;
         [SerializeField] Slider m_MissileSlider;
         [SerializeField] Slider m_ShiledSlider;
+
         [SerializeField] Button m_GunUpgradeBtn;
         [SerializeField] Button m_MagazineUpgrdBtn;
         [SerializeField] Button m_MissileAddBtn;
@@ -110,136 +110,123 @@ namespace praveen.One
 
         void Init()
         {
-            m_ShooterAmor = GameManager.Instance.GetAmorData();
+            int coinsInHand = GameManager.Instance.GetCoinCount();
+            m_ShooterAmor   = GameManager.Instance.GetAmorData();
 
-            m_GunPowerSlider.maxValue   = m_GunPowerDict.Count();
-            m_MagazineSlider.maxValue   = m_MissileMagazine.Count();
-            m_ShiledSlider.maxValue     = m_ShieldDict.Count();
-
-
-            m_GunPowerSlider.value  = m_ShooterAmor.GunLevel;
-            m_MagazineSlider.value  = m_ShooterAmor.MissileMagazineLvl;
-            m_Coins.text            = GameManager.Instance.GetCoinCount().ToString();
-
-            m_GunPowerLevel.text    = GetNextGunData(m_ShooterAmor.GunLevel);
-            m_RocketLevel.text      = GetNextGunData(m_ShooterAmor.MissileMagazineLvl);
+            m_Coins.text = coinsInHand.ToString();
 
             int nextGunPowerCost = GetNextGunCost(m_ShooterAmor.GunLevel);
             int nextMissileMagCost = GetNextMissileMagazineCost(m_ShooterAmor.MissileMagazineLvl);
-            
 
-            if (nextGunPowerCost != -1)
-            {
-                m_GunPowerCost.text = nextGunPowerCost.ToString();
 
-                // set the color of the text
-                if (nextGunPowerCost <= GameManager.Instance.GetCoinCount())
-                {
-                    m_GunPowerCost.color = Color.green;
-                    m_GunUpgradeBtn.interactable = true;
-                }
-                else
-                {
-                    m_GunPowerCost.color = Color.red;
-                    m_GunUpgradeBtn.interactable = false;
-                }
-                
-            }
+            //////
+            // Set Gun Power Related Stuff
+            //////
+            m_GunPowerSlider.maxValue   = m_GunPowerDict.Count();
+            m_GunPowerSlider.value      = m_ShooterAmor.GunLevel;
+            m_GunUpgradeBtn.interactable = SetButtonIntractable(nextGunPowerCost, coinsInHand);
+            m_GunPowerInfo.text = GetGunPowerInfoText(m_ShooterAmor.GunLevel);
 
-            if (nextMissileMagCost != -1)
-            {
-                m_RocketCost.text = nextMissileMagCost.ToString();
+            //////
+            // Set Missile Magazine Related Stuff
+            //////
+            m_MagazineSlider.maxValue   = m_MissileMagazine.Count();
+            m_MagazineSlider.value = m_ShooterAmor.MissileMagazineLvl;
+            m_MagazineUpgrdBtn.interactable = SetButtonIntractable(nextMissileMagCost,coinsInHand);
+            m_MagazineInfo.text = GetMagazineInfoText(m_ShooterAmor.MissileMagazineLvl);
 
-                // set the color of the text
-                if (nextMissileMagCost <= GameManager.Instance.GetCoinCount())
-                {
-                    m_MagazineUpgrdBtn.interactable = true;
-                    m_RocketCost.color = Color.green;
-                }
-                else
-                {
-                    m_MagazineUpgrdBtn.interactable = false;
-                    m_RocketCost.color = Color.red;
-                }
-            }
-
-            m_MissileCostTxt.text = m_MissileCost.ToString();
-            if(m_MissileCost <= GameManager.Instance.GetCoinCount())
-            {
-                m_MissileAddBtn.interactable = true;
-                m_MissileCostTxt.color = Color.green;
-            }
-            else
-            {
-                m_MissileAddBtn.interactable = false;
-                m_MissileCostTxt.color = Color.red;
-            }
-
+            //////
+            // Set Missile Related Stuff
+            //////
             int missileCount = m_ShooterAmor.MissileCount;
             int missileCap = GetMagazineCapacityBylvl(m_ShooterAmor.MissileMagazineLvl);
-            m_MissileCount.text = "Missile Count:"+ missileCount+"/"+ missileCap;
-
             m_MissileSlider.maxValue = missileCap;
             m_MissileSlider.value = missileCount;
+            m_MissileCount.text = "Missile Count:" + missileCount + "/" + missileCap;
+            m_MissileAddBtn.interactable = SetButtonIntractable(m_MissileCost, coinsInHand);
+            m_MissileInfoText.text = GetMissileCapacityInfoText();
 
+
+            //////
+            // Set Shield Related Stuff
+            //////
+            m_ShiledSlider.maxValue     = m_ShieldDict.Count();
             m_ShiledSlider.value = GetCurrentShieldLevel(GameManager.Instance.GetShieldDuration());
-
             int shieldLvl = GetCurrentShieldLevel(GameManager.Instance.GetShieldDuration());
             int shieldCost = GetNextShieldCost(shieldLvl);
-            m_ShieldLevel.text = GetNextShieldData(shieldLvl);
-            m_ShieldCost.text = shieldCost.ToString();
-
-            if(shieldCost <= GameManager.Instance.GetCoinCount())
-            {
-                m_ShieldCost.color = Color.green;
-                m_ShieldBtn.interactable = true;
-            }
-            else
-            {
-                m_ShieldCost.color = Color.red;
-                m_ShieldBtn.interactable = false;
-            }
+            m_ShieldInfo.text = GetShieldInfoText(shieldLvl);
+            m_ShieldBtn.interactable = SetButtonIntractable(shieldCost, coinsInHand);
 
         }
 
-        string GetNextGunData(int curLevel)
+        string GetGunPowerInfoText(int currentLvl)
         {
-            if (curLevel == m_GunPowerDict.OrderByDescending(x => x.Value).First().Key)
+            if (currentLvl == m_GunPowerDict.OrderByDescending(x => x.Value).First().Key)
             {
                 m_GunUpgradeBtn.interactable = false;
-                return "Already Upgraded to MAX level";
+                return "<color=#fd0000>Already Upgraded to MAX level</color>";
             }
-            return "Upgrade to Lvel "+ (curLevel +1) +" for:";
+            return "<color=#cfd2d4> Upgrade to Lvel " + (currentLvl + 1) + " for: </color>"+
+                GetCostString(m_GunPowerDict[currentLvl + 1]);
+            
+        }
+
+        string GetMagazineInfoText(int currentLvl)
+        {
+            if (currentLvl == m_MissileMagazine.OrderByDescending(x => x.Key).First().Key)
+            {
+                m_MagazineUpgrdBtn.interactable = false;
+                return "<color=#fd0000>Already Upgraded to MAX level</color>";
+            }
+            return "<color=#cfd2d4> Upgrade to Lvel " + (currentLvl + 1) + " for: </color>" +
+                GetCostString(m_MissileMagazine[currentLvl + 1].Cost);
+        }
+
+        string GetMissileCapacityInfoText()
+        {
+            if (GetMagazineCapacityBylvl(m_ShooterAmor.MissileMagazineLvl) <= m_ShooterAmor.MissileCount)
+            {
+                m_MissileAddBtn.interactable = false;
+                return "<color=#fd0000>Storage Already Full !</color>";
+            }
+            return "<color=#cfd2d4> Buy One Missile for:</color>" +
+                GetCostString(m_MissileCost);
+        }
+
+        string GetShieldInfoText(int currentLvl)
+        {
+            int shieldLvl = GetCurrentShieldLevel(GameManager.Instance.GetShieldDuration());
+            int shieldCost = GetNextShieldCost(shieldLvl);
+
+            if (currentLvl == m_ShieldDict.OrderByDescending(x => x.Key).First().Key)
+            {
+                m_ShieldBtn.interactable = false;
+                return "<color=#fd0000>Already Upgraded to MAX level</color>";
+            }
+            return "<color=#cfd2d4> Upgrade to " + m_ShieldDict[(currentLvl + 1)].Duration + " sec. for :</color>" +
+                GetCostString(shieldCost);
+        }
+
+        
+
+        string GetCostString(int upgradeCost)
+        {
+            if(upgradeCost <= GameManager.Instance.GetCoinCount())
+            {
+                return "<color=#00dd25>" + upgradeCost + "</color>"; // green
+            }
+            return "<color=#fd0000>" + upgradeCost + "</color>";  // red
         }
 
         int GetNextGunCost(int curLevel)
         {
-            if(curLevel == m_GunPowerDict.OrderByDescending(x => x.Value).First().Key)
+            if (curLevel == m_GunPowerDict.OrderByDescending(x => x.Value).First().Key)
             {
                 return -1;
             }
-            return m_GunPowerDict[curLevel+1];
+            return m_GunPowerDict[curLevel + 1];
         }
 
-        string GetNextMissileMagazineData(int curLevel)
-        {
-            if (curLevel == m_MissileMagazine.OrderByDescending(x => x.Value).First().Key)
-            {
-                m_MagazineUpgrdBtn.interactable = false;
-                return "Already Upgraded to MAX level";
-            }
-            return "Upgrade to Lvel " + (curLevel + 1) + " for:";
-        }
-
-        string GetNextShieldData(int curLevel)
-        {
-            if (curLevel == m_ShieldDict.OrderByDescending(x => x.Key).First().Key)
-            {
-                m_ShieldBtn.interactable = false;
-                return "Already Upgraded to MAX level";
-            }
-            return "Upgrade to " + m_ShieldDict[(curLevel + 1)].Duration + " sec. for:";
-        }
 
         int GetNextMissileMagazineCost(int curLevel)
         {
@@ -267,6 +254,12 @@ namespace praveen.One
         public void OnClickBackToGame()
         {
             SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+
+
+        bool SetButtonIntractable(int cost, int coinsInHand)
+        {
+            return cost <= coinsInHand;
         }
 
         public void OnClickUpgradeGun()
